ab9dab9503db6ba2b6c7d156a512423b830cf187
ThiagoDentinho <thiago.f.felipe@ufv.br>
2023-05-14 23:34:31 -0300

    primeiro-commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$template = file_get_contents('app/views/home.html');
+
+// class Citizen {
+//     private $name;
+//     private $nis;
+
+//     public function __construct($name, $nis) 
+//     {
+//         $this->name = $name;
+//         $this->nis = $nis;
+//     }
+
+//     public function getName() 
+//     {
+//         return $this->name;
+//     }
+
+//     public function getNis() {
+//         return $this->nis;
+//     }
+
+//     public function setName($name)
+//     {
+//         $this->name = $name;
+//     }
+
+//     public function store($name, $nis)
+//     {
+        
+//     }
+
+// }
